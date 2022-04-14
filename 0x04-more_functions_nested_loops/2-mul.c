8ba286fe69045f499d782cdff077ee678fa39cf8
Your Name <you@example.com>
2022-04-14 12:14:44 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -15,26 +15,3 @@ int mul(int a, int b)
 	return (c);
 }
 
-
-Task 3 3-print_numbers.c
-
-Ans: 
-
-#include "main.h"
-
-/**
- * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
- */
-void print_numbers(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
-
-	_putchar('\n');
-}
-
